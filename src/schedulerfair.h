a6c72f2be2bce8947c9d5698998f90f6b6126316
Pranith Kumar <bobby.prani@gmail.com>
2014-03-12 12:06:37 -0400

    Add missing files

---
+++ src/schedulerfair.h
@@ -0,0 +1,9 @@
+#ifndef __SCHEDULER_H__
+#define __SCHEDULER_H__
+
+void init_scheduler_vars(); //called from main
+void scheduler_stats(); //called from main
+void schedule(int); // scheduler function called every cycle
+
+#endif //__SCHEDULER_H__
+
